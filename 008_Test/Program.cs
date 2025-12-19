76b23bddb2b33a321f0628e31703bd0a298386d5
Shrek75 <freshine0@gmail.com>
2025-12-19 16:20:22 +0900

    Update Program.cs

--- 008_Test/Program.cs
+++ 008_Test/Program.cs
@@ -13,8 +13,53 @@ namespace _008_Test
         static void Main(string[] args)
         {
 
-            var test = Enumerable.Range(1, 10);
-            IEnumerable<int> test2 = Enumerable.Range(1, 100);
+            Nullable<int> i1 = null;
+            int? i2 = null;
+
+            Console.WriteLine(i1.HasValue);
+           // Console.WriteLine(i1.Value);
+            Console.WriteLine(i2.HasValue);
+            //   Console.WriteLine(i2.Value);
+
+            string s1 = null;
+            //   string? s2 = null;
+            if (s1 == null) ;
+            string s3 = s1 ?? "하이욤";
+            Console.WriteLine(s3);
+
+            int? x = null;
+            int y = x ?? default;
+
+
+            bool? unknown = null;
+            if (unknown ?? true)
+                Console.WriteLine("히히 NULL이거나 true였지롱");
+            unknown = true;
+            if(!unknown ?? true)
+                Console.WriteLine("히히 근데 우선순위가 뭐부터지");
+
+            double? d = 1.5;
+            Console.WriteLine(d?.ToString());
+            Console.WriteLine(d?.ToString("#.00"));
+
+            int? len;
+            string message;
+
+            message = null;
+            len = message?.Length ?? 0;
+            Console.WriteLine(len);
+
+
+            message = "안녕";
+            len = message?.Length;
+            Console.WriteLine(len);
+            Console.Write("a");
+            Console.Write("a");
+
+            int[] numbers = { 1, 2, 3 };
+            numbers.Sum();
+
+            
         }
 
     }
