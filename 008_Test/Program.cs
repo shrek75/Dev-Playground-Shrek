185a5dd5b928ef42cef2ee6cfb26d72841b9e0fa
Shrek75 <freshine0@gmail.com>
2025-12-19 19:29:09 +0900

    Update Program.cs

--- 008_Test/Program.cs
+++ 008_Test/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,53 +14,31 @@ namespace _008_Test
         static void Main(string[] args)
         {
 
-            Nullable<int> i1 = null;
-            int? i2 = null;
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            List<int> newNumbers = numbers.Where(n => n > 3).ToList();
+            foreach (int n in newNumbers)
+            {
+                Console.WriteLine(n);
+            }
 
-            Console.WriteLine(i1.HasValue);
-           // Console.WriteLine(i1.Value);
-            Console.WriteLine(i2.HasValue);
-            //   Console.WriteLine(i2.Value);
+            List<int> nn = new List<int> {1,2,3,4 };
+            Console.WriteLine(nn.Where(n => n % 2 == 0).Sum());
 
-            string s1 = null;
-            //   string? s2 = null;
-            if (s1 == null) ;
-            string s3 = s1 ?? "하이욤";
-            Console.WriteLine(s3);
+            var newList = new List<int> { 1, 2, 3, 4 };
+            Console.WriteLine(newList.Where(n=>n%2 ==1).Sum());
 
-            int? x = null;
-            int y = x ?? default;
+            int[] arr = { 1, 2, 3, 4, 5 };
+            IEnumerable<int> t = arr.Where(a => a % 2 == 1);
+            foreach(var n in t)
+            { Console.Write(n); }
 
+            bool[] blns = { true, false, true, false , true};
 
-            bool? unknown = null;
-            if (unknown ?? true)
-                Console.WriteLine("히히 NULL이거나 true였지롱");
-            unknown = true;
-            if(!unknown ?? true)
-                Console.WriteLine("히히 근데 우선순위가 뭐부터지");
+            Console.WriteLine(blns.Length);
+            Console.WriteLine(blns.Count());
+            Console.WriteLine(blns.Count(b=>b == true));
+            Console.WriteLine(blns.Count(b => b == false)                                               );
 
-            double? d = 1.5;
-            Console.WriteLine(d?.ToString());
-            Console.WriteLine(d?.ToString("#.00"));
-
-            int? len;
-            string message;
-
-            message = null;
-            len = message?.Length ?? 0;
-            Console.WriteLine(len);
-
-
-            message = "안녕";
-            len = message?.Length;
-            Console.WriteLine(len);
-            Console.Write("a");
-            Console.Write("a");
-
-            int[] numbers = { 1, 2, 3 };
-            numbers.Sum();
-
-            
         }
 
     }
