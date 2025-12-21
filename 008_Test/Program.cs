4c923b428f031bfa9909afdd7790b9b541e27b77
Shrek75 <freshine0@gmail.com>
2025-12-21 19:47:37 +0900

    Update Program.cs

--- 008_Test/Program.cs
+++ 008_Test/Program.cs
@@ -11,48 +11,25 @@ namespace _008_Test
 {
     internal class Program
     {
+
+        static IEnumerable<int> FindNumbers(Predicate<int> predicate)
+        {
+            for(int i= 0; i < 100; i++)
+            {
+                if (predicate(i)) yield return i;
+            }
+        }
+
         static void Main(string[] args)
         {
-
-            MyClass c = new MyClass();
-            c.MyProperty = 1;
-            Console.WriteLine(c.MyProperty);
-            c.MyProp = 2;
-            Console.WriteLine(c.MyProp);
-          //  c.MyProp2 = 3;
-
-
-
-            Console.WriteLine(c.MyProp2);
-            Console.WriteLine(MyClass.Name);
-
-            var anony = new { name = "김태욱", age = 27, height = 166 };
-            Console.WriteLine(anony.name);
+           var Numbers = FindNumbers((int i) => i % 33 == 0 );
+           foreach(int i in Numbers)
+                Console.WriteLine(i);
         }
-        static void Hello() => Console.WriteLine("Hello");
-        static int Doub(int x) => x * 2;
         
         
         
     }
-    class MyClass
-    {
-        public int MyProperty { get; set; }
-        private int MyValue { get; set; }
-
-        private int myprop = 4; 
-        public int MyProp
-        {
-            get { return myprop; }
-            set {  myprop = value; }
-        }
-
-        public static string Name { get; set; } = "Hello";
-
-        public int MyProp2 { get; } = 3;
-
-        public int Age { get; private set; }
-        
-        
-    }
+    
+    
 }
