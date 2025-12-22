608237acf25dd850856d31040fe632de2f75eb4e
Shrek75 <freshine0@gmail.com>
2025-12-22 21:18:00 +0900

    committ

--- 007_Test/007_Test.cpp
+++ 007_Test/007_Test.cpp
@@ -1,16 +1,15 @@
-﻿
-#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define LEN_INPUT 11
 
-int Func(char p[])
-{
-    printf("%s\n", p);
+int main(void) {
+    char s1[LEN_INPUT];
+    scanf("%s", s1);
+    for (int i = 0; i < LEN_INPUT; i++)
+    {
+        if (s1[i] == '\0') break;
+        printf("%c\n", s1[i]);
+    }
+
     return 0;
 }
-int main()
-{
-    char arr[3][10] = { "Hello","MyNameis","TaeUk" };
-
-    Func(arr[2]);
-    
-}
-
--- 008_Test/Program.cs
+++ 008_Test/Program.cs
@@ -9,26 +9,70 @@ using System.Threading.Tasks;
 
 namespace _008_Test
 {
+
     internal class Program
     {
 
-        static IEnumerable<int> FindNumbers(Predicate<int> predicate)
-        {
-            for(int i= 0; i < 100; i++)
-            {
-                if (predicate(i)) yield return i;
-            }
-        }
 
         static void Main(string[] args)
         {
-           var Numbers = FindNumbers((int i) => i % 33 == 0 );
-           foreach(int i in Numbers)
-                Console.WriteLine(i);
+            Random random = new Random();
+
+            //로또 당첨번호 만들기
+            int[] lotto = new int[6];
+            for(int i = 0; i < 6; i++)
+            {
+                lotto[i] = random.Next(1, 46);
+                for(int j=0;j<i;j++)
+                {
+                    if (lotto[j] == lotto[i])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+            //로또번호 정렬
+            var iter1 = lotto.OrderBy(x => x);
+   
+            int count = 0; //몇번 반복하는지 count
+
+            //1등당첨될때까지 반복
+            while (true)
+            {
+                count++;
+                if(count % 1_000_000 == 0) Console.WriteLine($"{count:#,#}회 구매했습니다.");
+
+                // 내 로또번호 뽑기
+                int[] myLotto = new int[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    myLotto[i] = random.Next(1, 46);
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (myLotto[j] == myLotto[i])
+                        {
+                            i--;
+                            break;
+                        }
+                    }
+                }
+
+                //내 로또번호 정렬
+                var iter2 = myLotto.OrderBy(x => x);
+                iter2.ElementAt(0);
+
+                //비교
+                if (iter1.SequenceEqual(iter2)) break;
+            }
+
+            Console.WriteLine($"count : {count}");
+            Console.WriteLine($"{((long)1000 * count):c} 원 사용");
+
         }
-        
-        
-        
+
+
+
     }
     
     
