903ad220ebee914b5002c04e96c5e2770c87ebda
Shrek75 <freshine0@gmail.com>
2025-12-22 22:37:47 +0900

    lotto

--- 009_Test/Program.cs
+++ 009_Test/Program.cs
@@ -10,6 +10,22 @@ namespace _009_Test
     {
         static void Main(string[] args)
         {
+            MyClass m = new MyClass();
+            Console.WriteLine(m.MyMoney);
+            m.MyHealth = 2;
+            m.height = 3;
         }
+    }
+    class MyClass
+    {
+        int myMoney = 0;
+        public int MyMoney
+        {
+            get { return myMoney; }
+            set { myMoney = value; }
+        }
+        public int MyHealth { get; set; } = 100;
+
+        public int height = 200;
     }
 }
