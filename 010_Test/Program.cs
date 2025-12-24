4376948a98e6826ef0914a2db0d5a33b91c4a4cf
Shrek75 <freshine0@gmail.com>
2025-12-24 22:52:14 +0900

    f

--- 010_Test/Program.cs
+++ 010_Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace _010_Test
     {
         static void Main(string[] args)
         {
+            Past past = new Future();
+            past.Func();
+        }
+
+        
+        
+    }
+
+    public class Past
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("안녕하세요");
+        }
+
+        public void Func()
+        {
+            Print();
         }
     }
+
+    public class Future : Past
+    {
+        public override void  Print() 
+        {
+            Console.WriteLine("안냥~?");
+        }
+    }
+    
 }
